1926c4e9ffa5e788815138541b7b62aed7d7d20c
hectormrc <thehector1593@gmail.com>
2025-03-22 13:56:11 +0100

    test: add test for determinant sign

--- src/determinant.rs
+++ src/determinant.rs
@@ -2,16 +2,16 @@ use std::ops::{Mul, Sub};
 
 use crate::{point::Point, polygon::Segment};
 
-/// The scalar value representing the determinant between two vectors.
-#[derive(Debug, Copy, Clone)]
+/// The scalar value representing the determinant of a matrix.
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 pub struct Determinant<T>(T);
 
 impl<T> From<[&Point<T>; 3]> for Determinant<T>
 where
     T: Copy + Sub<Output = T> + Mul<Output = T>,
 {
-    /// Being `A`, `B` and `C` the given [`Point`]s, returns the determinant of the direction
-    /// vectors `AB` and `AC`.
+    /// Being `A`, `B` and `C` the given [`Point`]s, returns the determinant of the matrix
+    /// representing the direction vectors `AB` and `AC`.
     fn from([a, b, c]: [&Point<T>; 3]) -> Self {
         Self((b.x - a.x) * (c.y - a.y) - (c.x - a.x) * (b.y - a.y))
     }
@@ -22,7 +22,7 @@ where
     T: Copy + Sub<Output = T> + Mul<Output = T>,
 {
     /// Being `A` and `B` the endpoints of the given [`Segment`], and `C` the given [`Point`],
-    /// returns the determinant of the direction vector `AB` and `AC`.
+    /// returns the determinant of the matrix representing the direction vector `AB` and `AC`.
     fn from((segment, point): (&Segment<'_, T>, &Point<T>)) -> Self {
         Self::from([&segment.from, &segment.to, point])
     }
@@ -32,7 +32,8 @@ impl<T> From<[&Segment<'_, T>; 2]> for Determinant<T>
 where
     T: Copy + Sub<Output = T> + Mul<Output = T>,
 {
-    /// Returns the determinant of the direction vectors of the given [`Segment`]s.
+    /// Returns the determinant of the matrix representing the direction vectors of the given
+    /// [`Segment`]s.
     fn from([a, b]: [&Segment<'_, T>; 2]) -> Self {
         Self((a.to.x - a.from.x) * (b.to.y - b.from.y) - (b.to.x - b.from.x) * (a.to.y - a.from.y))
     }
@@ -42,5 +43,46 @@ impl<T> Determinant<T> {
     /// Returns the inner value of self.
     pub fn into_inner(self) -> T {
         self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{
+        determinant::Determinant,
+        point::{Point, point},
+    };
+
+    #[test]
+    fn determinant_of_vectors() {
+        struct Test {
+            name: &'static str,
+            points: [Point<f32>; 3],
+            want: Determinant<f32>,
+        }
+
+        vec![
+            Test {
+                name: "Clockwise vectors",
+                points: [point!(0., 1.), point!(0., 0.), point!(-1., 0.)],
+                want: Determinant(-1.),
+            },
+            Test {
+                name: "Counter-clockwise vectors",
+                points: [point!(0., 1.), point!(0., 0.), point!(1., 0.)],
+                want: Determinant(1.),
+            },
+        ]
+        .into_iter()
+        .for_each(|test| {
+            let [a, b, c] = test.points;
+            let got = Determinant::from([&a, &b, &c]);
+
+            assert_eq!(
+                got, test.want,
+                "{}: got determinant = {got:?}, want = {:?}",
+                test.name, test.want
+            );
+        });
     }
 }
