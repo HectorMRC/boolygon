80a34f41ae0a02716076aa4a0cf43498b4a9b419
hectormrc <thehector1593@gmail.com>
2025-05-16 17:30:45 +0200

    refactor: split clipper module

--- src/clipper/graph.rs
+++ src/clipper/graph.rs
@@ -1,193 +1,10 @@
-use std::{cmp::Ordering, collections::BTreeMap, fmt::Debug, marker::PhantomData};
+use std::{cmp::Ordering, collections::BTreeMap, fmt::Debug};
 
 use num_traits::{Float, Signed};
 
 use crate::{point::Point, polygon::Segment, shape::Shape};
 
-pub struct Unknown;
-
-pub(crate) struct Operands<'a, T> {
-    pub subject: &'a Shape<T>,
-    pub clip: &'a Shape<T>,
-}
-
-impl<'a, T, Op> From<&'a Clipper<Op, Shape<T>, Shape<T>>> for Operands<'a, T> {
-    fn from(clipper: &'a Clipper<Op, Shape<T>, Shape<T>>) -> Self {
-        Operands {
-            subject: &clipper.subject,
-            clip: &clipper.clip,
-        }
-    }
-}
-
-/// Represents the operation to perform by the clipping algorithm.
-pub(crate) trait Operator<T> {
-    /// Returns true if, and only if, the given vertex belongs to the output of the clipping
-    /// operation.
-    fn is_output<'a>(ops: Operands<'a, T>, vertex: &'a Vertex<T>) -> bool;
-}
-
-/// Implements the clipping algorithm.                                                                                                                                   
-pub(crate) struct Clipper<Operator, Subject, Clip> {
-    operator: PhantomData<Operator>,
-    subject: Subject,
-    clip: Clip,
-}
-
-impl Default for Clipper<Unknown, Unknown, Unknown> {
-    fn default() -> Self {
-        Self {
-            operator: PhantomData,
-            subject: Unknown,
-            clip: Unknown,
-        }
-    }
-}
-
-impl<Op, Sub, Clip> Clipper<Op, Sub, Clip> {
-    pub fn with_operator<Operator>(self) -> Clipper<Operator, Sub, Clip> {
-        Clipper {
-            operator: PhantomData,
-            subject: self.subject,
-            clip: self.clip,
-        }
-    }
-}
-
-impl<Op, Clip> Clipper<Op, Unknown, Clip> {
-    pub fn with_subject<T>(self, subject: impl Into<Shape<T>>) -> Clipper<Op, Shape<T>, Clip> {
-        Clipper {
-            operator: PhantomData,
-            subject: subject.into(),
-            clip: self.clip,
-        }
-    }
-}
-
-impl<Op, Sub> Clipper<Op, Sub, Unknown> {
-    pub fn with_clip<T>(self, clip: impl Into<Shape<T>>) -> Clipper<Op, Sub, Shape<T>> {
-        Clipper {
-            operator: PhantomData,
-            subject: self.subject,
-            clip: clip.into(),
-        }
-    }
-}
-
-impl<T, Op> Clipper<Op, Shape<T>, Shape<T>>
-where
-    T: Clone + PartialOrd + Signed + Float + Debug,
-    Op: Operator<T>,
-{
-    pub fn execute(self) -> Option<Shape<T>> {
-        let mut graph = ClipperGraph::default()
-            .with_subject(self.subject.clone())
-            .with_clip(self.clip.clone())
-            .with_intersections();
-
-        let mut shape = None;
-        while let Some(iter) = graph
-            .position_where(Vertex::is_intersection)
-            .map(|position| VerticesIterator {
-                clipper: &self,
-                graph: &mut graph,
-                next: position,
-            })
-        {
-            let polygon = iter.map(|vertex| vertex.point).collect::<Vec<_>>().into();
-            match shape.as_mut() {
-                None => shape = Some(Shape::from(polygon)),
-                Some(shape) => shape.polygons.push(polygon),
-            };
-        }
-
-        while let Some(iter) = graph
-            .position_where(|vertex| Op::is_output((&self).into(), vertex))
-            .map(|position| VerticesIterator {
-                clipper: &self,
-                graph: &mut graph,
-                next: position,
-            })
-        {
-            let start = iter.next;
-            let vertices = iter.collect::<Vec<_>>();
-
-            if vertices[vertices.len() - 1].next != start {
-                // The succession of vertices is an open shape.
-                continue;
-            }
-
-            let polygon = vertices
-                .into_iter()
-                .map(|vertex| vertex.point)
-                .collect::<Vec<_>>()
-                .into();
-
-            match shape.as_mut() {
-                None => shape = Some(Shape::from(polygon)),
-                Some(shape) => shape.polygons.push(polygon),
-            };
-        }
-
-        shape
-    }
-}
-
-#[derive(Debug)]
-pub(crate) struct Vertex<T> {
-    /// The location of the vertex.
-    pub point: Point<T>,
-    /// The index of the vertex following this one.
-    next: usize,
-    /// The index of the vertex previous to this one.
-    previous: usize,
-    /// Vertices from the oposite shape located at the same point.
-    siblings: Vec<usize>,
-}
-
-impl<T> Vertex<T> {
-    fn is_intersection(&self) -> bool {
-        !self.siblings.is_empty()
-    }
-}
-
-struct VerticesIterator<'a, Op, T> {
-    clipper: &'a Clipper<Op, Shape<T>, Shape<T>>,
-    graph: &'a mut ClipperGraph<T>,
-    next: usize,
-}
-
-impl<'a, Op, T> Iterator for VerticesIterator<'a, Op, T>
-where
-    T: Signed + Float,
-    Op: Operator<T>,
-{
-    type Item = Vertex<T>;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        let vertex = self.graph.vertices[self.next].take()?;
-
-        self.next = vertex
-            .siblings
-            .iter()
-            .find_map(|&sibling| {
-                if self.graph.vertices[sibling]
-                    .as_ref()
-                    .and_then(|vertex| self.graph.vertices[vertex.next].as_ref())
-                    .is_some_and(|vertex| Op::is_output(self.clipper.into(), vertex))
-                {
-                    return self.graph.vertices[sibling]
-                        .take()
-                        .map(|vertex| vertex.next);
-                }
-
-                None
-            })
-            .unwrap_or(vertex.next);
-
-        Some(vertex)
-    }
-}
+use super::vertex::Vertex;
 
 /// The index of the first [`Vertex`] of a [`Polygon`] belonging to the clip or subject [`Shape`].
 #[derive(Debug)]
@@ -209,7 +26,7 @@ struct Edge<'a, T> {
 }
 
 struct EdgesIterator<'a, T> {
-    graph: &'a ClipperGraph<T>,
+    graph: &'a Graph<T>,
     next: Option<usize>,
     start: usize,
 }
@@ -259,16 +76,16 @@ impl<T> Default for Intersections<T> {
     }
 }
 
-impl<T> From<&ClipperGraph<T>> for Intersections<T>
+impl<T> From<&GraphBuilder<T>> for Intersections<T>
 where
     T: Signed + Float,
 {
-    fn from(graph: &ClipperGraph<T>) -> Self {
+    fn from(builder: &GraphBuilder<T>) -> Self {
         let mut intersections = Self::default();
-        for subject_polygon in graph.polygons.iter().filter(|p| p.is_subject()) {
-            for clip_polygon in graph.polygons.iter().filter(|p| !p.is_subject()) {
-                for subject_edge in graph.edges(subject_polygon) {
-                    for clip_edge in graph.edges(clip_polygon) {
+        for subject_polygon in builder.polygons.iter().filter(|p| p.is_subject()) {
+            for clip_polygon in builder.polygons.iter().filter(|p| !p.is_subject()) {
+                for subject_edge in builder.graph.edges(subject_polygon) {
+                    for clip_edge in builder.graph.edges(clip_polygon) {
                         if let Some(intersection) =
                             subject_edge.segment.intersection(&clip_edge.segment)
                         {
@@ -352,30 +169,66 @@ impl<K, V> PartialOrdBTreeMap<K, V> {
 }
 
 #[derive(Debug)]
-struct ClipperGraph<T> {
-    vertices: Vec<Option<Vertex<T>>>,
-    polygons: Vec<Boundary>,
+pub(super) struct Graph<T> {
+    pub(super) vertices: Vec<Option<Vertex<T>>>,
 }
 
-impl<T> Default for ClipperGraph<T> {
+impl<T> Default for Graph<T> {
     fn default() -> Self {
         Self {
             vertices: Default::default(),
+        }
+    }
+}
+
+impl<T> Graph<T> {
+    fn edges(&self, boundary: &Boundary) -> impl Iterator<Item = Edge<'_, T>> {
+        let start = match boundary {
+            Boundary::Clip(index) | Boundary::Subject(index) => *index,
+        };
+
+        EdgesIterator {
+            graph: self,
+            start,
+            next: Some(start),
+        }
+    }
+
+    pub(super) fn position_where(&self, f: impl Fn(&Vertex<T>) -> bool) -> Option<usize> {
+        self.vertices
+            .iter()
+            .enumerate()
+            .filter_map(|(index, cell)| cell.as_ref().map(|vertex| (index, vertex)))
+            .find(|(_, vertex)| f(vertex))
+            .map(|(start, _)| start)
+    }
+}
+
+#[derive(Debug)]
+pub(super) struct GraphBuilder<T> {
+    graph: Graph<T>,
+    polygons: Vec<Boundary>,
+}
+
+impl<T> Default for GraphBuilder<T> {
+    fn default() -> Self {
+        Self {
+            graph: Default::default(),
             polygons: Default::default(),
         }
     }
 }
 
-impl<T> ClipperGraph<T>
+impl<T> GraphBuilder<T>
 where
     T: PartialOrd + Signed + Float + Debug,
 {
-    fn with_intersections(mut self) -> Self {
+    pub(super) fn build(mut self) -> Graph<T> {
         let intersections = Intersections::from(&self);
         let mut visited = PartialOrdBTreeMap::new();
 
         for (edge, mut indexes) in intersections.by_edge {
-            let vertex = self.vertices[edge]
+            let vertex = self.graph.vertices[edge]
                 .as_ref()
                 .expect("edge vertex should exist")
                 .point;
@@ -391,21 +244,21 @@ where
                 .chunk_by(|&a, &b| intersections.all[a].point == intersections.all[b].point)
                 .fold(edge, |previous, chunk| {
                     let point = intersections.all[chunk[0]].point;
-                    let index = self.vertices.len();
+                    let index = self.graph.vertices.len();
 
                     visited.insert((edge, point), index);
 
-                    let next = self.vertices[previous]
+                    let next = self.graph.vertices[previous]
                         .as_ref()
                         .expect("previous vertex should exist")
                         .next;
 
-                    self.vertices[previous]
+                    self.graph.vertices[previous]
                         .as_mut()
                         .expect("previous vertex should exist")
                         .next = index;
 
-                    self.vertices[next]
+                    self.graph.vertices[next]
                         .as_mut()
                         .expect("next vertex should exist")
                         .previous = index;
@@ -422,7 +275,7 @@ where
                         .filter_map(|edge| visited.get((edge, point)))
                         .copied()
                         .inspect(|&sibling| {
-                            self.vertices[sibling]
+                            self.graph.vertices[sibling]
                                 .as_mut()
                                 .expect("sibling should exist")
                                 .siblings
@@ -430,7 +283,7 @@ where
                         })
                         .collect();
 
-                    self.vertices.push(Some(Vertex {
+                    self.graph.vertices.push(Some(Vertex {
                         point,
                         next,
                         previous,
@@ -441,32 +294,32 @@ where
                 });
         }
 
-        self
+        self.graph
     }
 }
 
-impl<T> ClipperGraph<T> {
-    fn with_subject(self, shape: Shape<T>) -> Self {
+impl<T> GraphBuilder<T> {
+    pub(super) fn with_subject(self, shape: Shape<T>) -> Self {
         self.with_shape(shape, Boundary::Subject)
     }
 
-    fn with_clip(self, shape: Shape<T>) -> Self {
+    pub(super) fn with_clip(self, shape: Shape<T>) -> Self {
         self.with_shape(shape, Boundary::Clip)
     }
 
     fn with_shape(mut self, shape: Shape<T>, boundary: impl Fn(usize) -> Boundary) -> Self {
-        self.vertices.reserve(shape.total_vertices());
+        self.graph.vertices.reserve(shape.total_vertices());
         self.polygons.reserve(shape.polygons.len());
 
         for polygon in shape.polygons {
-            let offset = self.vertices.len();
+            let offset = self.graph.vertices.len();
             self.polygons.push(boundary(offset));
 
             let total_vertices = polygon.vertices.len();
             for (mut index, vertex) in polygon.vertices.into_iter().enumerate() {
                 index += total_vertices;
 
-                self.vertices.push(Some(Vertex {
+                self.graph.vertices.push(Some(Vertex {
                     point: vertex,
                     next: offset + ((index + 1) % total_vertices),
                     previous: offset + ((index - 1) % total_vertices),
@@ -476,26 +329,5 @@ impl<T> ClipperGraph<T> {
         }
 
         self
-    }
-
-    fn edges(&self, boundary: &Boundary) -> impl Iterator<Item = Edge<'_, T>> {
-        let start = match boundary {
-            Boundary::Clip(index) | Boundary::Subject(index) => *index,
-        };
-
-        EdgesIterator {
-            graph: self,
-            start,
-            next: Some(start),
-        }
-    }
-
-    fn position_where(&self, f: impl Fn(&Vertex<T>) -> bool) -> Option<usize> {
-        self.vertices
-            .iter()
-            .enumerate()
-            .filter_map(|(index, cell)| cell.as_ref().map(|vertex| (index, vertex)))
-            .find(|(_, vertex)| f(vertex))
-            .map(|(start, _)| start)
     }
 }
---
+++ src/clipper/vertex.rs
@@ -0,0 +1,63 @@
+use std::fmt::Debug;
+
+use num_traits::{Float, Signed};
+
+use crate::{point::Point, shape::Shape};
+
+use super::{Clipper, Operator, graph::Graph};
+
+#[derive(Debug)]
+pub struct Vertex<T> {
+    /// The location of the vertex.
+    pub point: Point<T>,
+    /// The index of the vertex following this one.
+    pub(super) next: usize,
+    /// The index of the vertex previous to this one.
+    pub(super) previous: usize,
+    /// Vertices from the oposite shape located at the same point.
+    pub(super) siblings: Vec<usize>,
+}
+
+impl<T> Vertex<T> {
+    pub(super) fn is_intersection(&self) -> bool {
+        !self.siblings.is_empty()
+    }
+}
+
+pub(super) struct VerticesIterator<'a, Op, T> {
+    pub(super) clipper: &'a Clipper<Op, Shape<T>, Shape<T>>,
+    pub(super) graph: &'a mut Graph<T>,
+    pub(super) next: usize,
+}
+
+impl<'a, Op, T> Iterator for VerticesIterator<'a, Op, T>
+where
+    T: Signed + Float,
+    Op: Operator<T>,
+{
+    type Item = Vertex<T>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let vertex = self.graph.vertices[self.next].take()?;
+
+        self.next = vertex
+            .siblings
+            .iter()
+            .find_map(|&sibling| {
+                if self.graph.vertices[sibling]
+                    .as_ref()
+                    .and_then(|vertex| self.graph.vertices[vertex.next].as_ref())
+                    .is_some_and(|vertex| Op::is_output(self.clipper.into(), vertex))
+                {
+                    return self.graph.vertices[sibling]
+                        .take()
+                        .map(|vertex| vertex.next);
+                }
+
+                None
+            })
+            .unwrap_or(vertex.next);
+
+        Some(vertex)
+    }
+}
